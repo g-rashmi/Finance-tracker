2b8a7c95ce1bf223138780155eef5ab78353bf6c
g-rashmi <rashmi.gupta7018@gmail.com>
2024-09-02 07:33:20 +0530

    modiff

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 
-import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
 import SignIn from './components/Signin';
 import Signup from './components/Signup';
 import Dashboard from './components/Dashboard';
